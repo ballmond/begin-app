bab3c6a2ff2591294893691f60a22347803ced0c
Robert Allmond <ballmond@gmail.com>
2020-12-23 17:49:29 -0500

    more tries

--- src/http/post-mailchimp-addListMember-000listId/index.js
+++ src/http/post-mailchimp-addListMember-000listId/index.js
@@ -5,8 +5,27 @@ mailchimp.setConfig({
   server: "us7",
 });
 
-async function addMember(listId, email, fname, lname) {
-  let response 
+// async function addMember(listId, email, fname, lname) {
+//   let response 
+//   try {
+//     response = await mailchimp.lists.addListMember(listId, {
+//       email_address: email,
+//       status: "subscribed",
+//       merge_fields: {
+//         FNAME: fname,
+//         LNAME: lname
+//     }})
+//     return response
+//   } catch (error) {
+//     return error
+//   }
+// }
+
+exports.handler = async function http(req) {
+  let { listId } = req.pathParameters
+  let {email, fname, lname} = req.queryStringParameters
+  let response
+
   try {
     response = await mailchimp.lists.addListMember(listId, {
       email_address: email,
@@ -15,35 +34,24 @@ async function addMember(listId, email, fname, lname) {
         FNAME: fname,
         LNAME: lname
     }})
-    return response
-  } catch (error) {
-    return error
-  }
-}
-
-exports.handler = async function http(req) {
-  let { listId } = req.pathParameters
-  let {email, fname, lname} = req.queryStringParameters
-  let response
-
-  try {
-    response = await addMember(listId, email, fname, lname)    
-    return {
-      statusCode: response.status === 'subscribed' ? 201 : response.status,
-      cors: true,
-      headers: {
-        'access-control-allow-origin': '*',
-        "access-control-allow-headers": ["Content-Type"],
-        "Content-type": "application/json; charset=UTF-8"
-      },
-      body: JSON.stringify({
-        listId: `${listId}`,
-        email: `${email}`,
-        fname: `${fname}`,
-        lname: `${lname}`,
-        response: response
-      })
-    }  
+    // response = await addMember(listId, email, fname, lname)    
+    // return {
+    //   statusCode: response.status === 'subscribed' ? 201 : response.status,
+    //   cors: true,
+    //   headers: {
+    //     'access-control-allow-origin': '*',
+    //     "access-control-allow-headers": ["Content-Type"],
+    //     "Content-type": "application/json; charset=UTF-8"
+    //   },
+    //   body: JSON.stringify({
+    //     listId: `${listId}`,
+    //     email: `${email}`,
+    //     fname: `${fname}`,
+    //     lname: `${lname}`,
+    //     response: response
+    //   })
+    // }  
+    return {statusCode: 200}
   } catch (error) {
     return {
       statusCode: 400,
