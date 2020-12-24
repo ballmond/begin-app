cba36baf579d0ab13fe55d970e9bea14c4a748b4
Robert Allmond <ballmond@gmail.com>
2020-12-23 21:15:08 -0500

    mod tests

--- test/api-test.js
+++ test/api-test.js
@@ -11,49 +11,50 @@ test('start', async t=> {
   t.ok(true, 'started')
 })
 
-test('get-index', async t => {
-  t.plan(1)
-  try {
-    let result = await tiny.get({url: `${base}`})
-    t.ok(result, 'Got response', console.log(result.body))
-  } catch (err) {
-    t.fail(err)
-  }
-})
+// test('get-index', async t => {
+//   t.plan(1)
+//   try {
+//     let result = await tiny.get({url: `${base}`})
+//     t.ok(result, 'Got response', console.log(result.body))
+//   } catch (err) {
+//     t.fail(err)
+//   }
+// })
 
-test('get-echo', async t => {
-  t.plan(1)
-  try {
-    let result = await tiny.get({url: `${base}/echo/praisecage`})
-    t.equal(result.body.echo, 'praisecage', 'echo matches')
-  } catch(err) {
-    t.fail(err)
-  }
-})
+// test('get-echo', async t => {
+//   t.plan(1)
+//   try {
+//     let result = await tiny.get({url: `${base}/echo/praisecage`})
+//     t.equal(result.body.echo, 'praisecage', 'echo matches')
+//   } catch(err) {
+//     t.fail(err)
+//   }
+// })
 
-test('get-json', async t => {
-  t.plan(1)
-  try {
-    let result = await tiny.get({url: `${base}/json`})
-    if (process.env.MESSAGE_STYLE) {
-      t.equal(result.body.message, 'HELLO JSON', 'json matches')
-    } else {
-      t.equal(result.body.message,'Hello json', 'json matches')
-    }
-  } catch(err) {
-    t.fail(err)
-  }
-})
+// test('get-json', async t => {
+//   t.plan(1)
+//   try {
+//     let result = await tiny.get({url: `${base}/json`})
+//     if (process.env.MESSAGE_STYLE) {
+//       t.equal(result.body.message, 'HELLO JSON', 'json matches')
+//     } else {
+//       t.equal(result.body.message,'Hello json', 'json matches')
+//     }
+//   } catch(err) {
+//     t.fail(err)
+//   }
+// })
 
-test('get-name', async t => {
-  t.plan(1)
-  try {
-    let result = await tiny.get({url: `${base}/name?first=nic&last=cage` })
-    t.equal(result.body.name, 'nic cage', 'query param name matches')
-  } catch(err) {
-    t.fail(err)
-  }
-})
+// test('get-name', async t => {
+//   t.plan(1)
+//   try {
+//     let result = await tiny.get({url: `${base}/name?first=nic&last=cage` })
+//     t.equal(result.body.name, 'nic cage', 'query param name matches')
+//   } catch(err) {
+//     t.fail(err)
+//   }
+// })
+
 
 // this ends sandbox
 test('end', async t=> {
